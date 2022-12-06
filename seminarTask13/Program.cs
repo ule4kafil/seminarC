38f557ef1779d143f0f5992481ae27c2b729ce0e
Julia <juliauniver145@gmail.com>
2022-12-07 00:10:42 +0300

    HomeworkC#2

---
+++ seminarTask13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет
+
+
+Console.Write("Введите число: "); 
+int i = int.Parse(Console.ReadLine()); 
+
+if (Math.Abs(i) <= 99) 
+{ 
+    Console.WriteLine("третьей цифры нет: ");
+}
+else
+{
+    string s1 = i.ToString(); 
+    if (i > 0) 
+        Console.WriteLine(s1[2]); 
+    else 
+        Console.WriteLine(s1[3]);
+
+}
---
+++ seminarTask15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Add number");
+
+int day = int.Parse(Console.ReadLine());
+
+if (day > 7)
+{
+    Console.WriteLine("error");
+    return;
+}
+
+if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5)
+{
+    Console.WriteLine("weekday");
+}
+else
+{
+    Console.WriteLine("weekend");
+}
+
+
