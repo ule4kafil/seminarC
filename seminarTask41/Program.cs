7838a894b154edad928db6500754f3d07a7482f6
Julia <juliauniver145@gmail.com>
2022-12-15 13:32:36 +0300

    add Homework6

---
+++ seminarTask41/Program.cs
@@ -0,0 +1,21 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.Write("Add numbers: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Result: {count}");
+
+
+
+
