36be763f3a1303c7b69bb144160c906b0eab9c9c
Julia <juliauniver145@gmail.com>
2022-12-17 14:04:06 +0300

    Homework8

---
+++ seminarTask47/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+//m = 3, n = 4.
+
+
+double[,] TwoDementionArray(int length, int secondlength)
+{
+    Random random = new Random();
+    double[,] array = new double[length, secondlength];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.NextSingle() * 100 * RandomSymbol();
+        }
+    }
+    return array;
+}
+
+int ReadInt()
+{
+    Console.WriteLine("input number: ");
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int RandomSymbol()
+{
+    Random random = new Random();
+    if (random.Next(10) > 5) return 1;
+    return -1;
+}
+
+void GetArrayAsString(double[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write(string.Format("{0:0.##} ", massive[i,j]));
+        }
+        Console.WriteLine();
+    }
+}
+double[,] massiv = TwoDementionArray(ReadInt(), ReadInt());
+Console.WriteLine();
+GetArrayAsString(massiv);
+
+
+
---
+++ seminarTask50/Program.cs
@@ -0,0 +1,68 @@
+﻿//Напишите программу, которая на вход принимает позиции элемента
+//в двумерном массиве, и возвращает значение этого элемента или же
+//указание, что такого элемента нет.
+//Например, задан массив:
+
+int[,] inputTwoDimensionArray(int length, int secondLength)
+{
+    int[,] mass = new int[length, secondLength];
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        FillDimention(i, mass);
+    }
+    return mass;
+}
+
+int ReadInt()
+{
+    Console.WriteLine("input number");
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+void FillDimention(int index, int[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(1); i++)
+    {
+        massive[index, i] = new Random().Next(1, 10);
+    }
+}
+
+void GetArrayAsString(int[,] massive)     
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] massive = inputTwoDimensionArray(5, 5);
+
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+if (m < massive.GetLength(0) && n < massive.GetLength(1))
+{
+    Console.WriteLine(massive[m, n]);
+}
+else
+{
+    Console.WriteLine("not exist");
+}
+
+
+Console.WriteLine(massive[m, n]);
+
+
+Task();
+
+void Task()
+{
+    int[,] array = inputTwoDimensionArray(ReadInt(), ReadInt());
+    GetArrayAsString(array);
+}
+
+
